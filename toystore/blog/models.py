fda1afdc3e4683ba89fe29e93fae10f844c974d3
CE9752 <fateme.ahmadi1522@gmail.com>
2024-02-16 00:35:02 +0330

    Define Category and Post models

--- toystore/blog/models.py
+++ toystore/blog/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=250, unique=True, null=False, blank=False, verbose_name="title")
+    description = models.TextField(null=False, blank=False, verbose_name="description")
+
+    class Meta:
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
+        ordering = ['id']
+
+    def __str__(self):
+        return self.title
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=250, null=False, blank=False, verbose_name="title")
+    description = models.TextField(null=False, blank=False, verbose_name="description")
+    category = models.ForeignKey(Category, related_name="posts", on_delete=models.CASCADE, verbose_name="category")
+
+    class Meta:
+        verbose_name = "Post"
+        verbose_name_plural = "Posts"
+        ordering = ['id']
+
+    def __str__(self):
+        return self.title
