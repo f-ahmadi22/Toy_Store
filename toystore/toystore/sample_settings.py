298d83c83eb7c6bfceb226cf49382f86118d5514
CE9752 <fateme.ahmadi1522@gmail.com>
2024-02-15 14:09:04 +0330

    Import project in Toy_store repo

---
+++ toystore/toystore/sample_settings.py
@@ -0,0 +1,14 @@
+DATABASE = {
+    'name': '',
+    'user': '',
+    'password': '',
+    'host': 'localhost',
+    'port': '5432',
+}
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = ''
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
