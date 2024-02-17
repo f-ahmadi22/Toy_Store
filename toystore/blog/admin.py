f341aa05c88069e05e0ecb528cb6a03cdf220e0d
CE9752 <fateme.ahmadi1522@gmail.com>
2024-02-17 11:24:07 +0330

    Create CategoryAdmin for admin panel

--- toystore/blog/admin.py
+++ toystore/blog/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from django.contrib.admin import register
+from .models import Category, Post, Comment, Media
 # Register your models here.
+
+
+@register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'description', 'is_active', 'created_at', 'updated_at')
+    list_display_links = ('id', 'title', 'description', 'is_active', 'created_at', 'updated_at')
+    list_filter = ('is_active',)
+    list_editable = ('is_active',)
+    search_fields = ('title', 'description')
