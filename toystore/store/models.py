3e22ae18c3849c428faaa1dcf6adbd58951cfa29
CE9752 <fateme.ahmadi1522@gmail.com>
2024-02-17 20:38:05 +0330

    Add Product model

--- toystore/store/models.py
+++ toystore/store/models.py
@@ -1,19 +1,18 @@
 from django.db import models
-from ..blog.models import MyBaseModel
+from ..blog.models import MyBaseModel, Category
 # Create your models here.
 
 
-class Category(MyBaseModel):
-    title = models.CharField(max_length=250, unique=True, null=False, blank=False, verbose_name="title")
+class Product(MyBaseModel):
+    title = models.CharField(max_length=250, null=False, blank=False, verbose_name="title")
     description = models.TextField(null=False, blank=False, verbose_name="description")
+    price = models.IntegerField(null=False, blank=False, verbose_name="price")
+    category = models.ForeignKey(Category, related_name="posts", on_delete=models.CASCADE, verbose_name="category")
 
     class Meta:
-        verbose_name = "Category"
-        verbose_name_plural = "Categories"
+        verbose_name = "Product"
+        verbose_name_plural = "Products"
         ordering = ['id']
 
     def __str__(self):
         return self.title
-
-
-
