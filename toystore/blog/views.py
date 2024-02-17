e37d908d2ab56e0c15c54928644a9640d504e702
CE9752 <fateme.ahmadi1522@gmail.com>
2024-02-17 12:57:55 +0330

    Add Category and Post viewsets

--- toystore/blog/views.py
+++ toystore/blog/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from typing import Tuple
 
+from django.shortcuts import render
+from rest_framework import viewsets, status, filters, permissions
+from django_filters.rest_framework import DjangoFilterBackend
+from .models import Post, Comment, Category, Media
+from .serializers import CategorySerializer, PostSerializer, CommentSerializer, MediaSerializer
 # Create your views here.
+
+
+class CategoryViewSet(viewsets.ReadOnlyModelViewSet):
+    serializer_class = CategorySerializer
+    queryset = Category.objects.filter(is_active=True).order_by('-pk')
+    # permission_classes = [permissions.IsAuthenticated]
+    filter_backends = (DjangoFilterBackend, filters.OrderingFilter, filters.SearchFilter)
+
+
+class PostViewSet(viewsets.ReadOnlyModelViewSet):
+    serializer_class = PostSerializer
+    # permission_classes = [permissions.IsAuthenticated]
+    queryset = Post.objects.filter(is_active=True).order_by('-pk')
+    filter_backends = (DjangoFilterBackend, filters.OrderingFilter, filters.SearchFilter)
+    filterset_fields = ('id', 'category')
+    search_fields = ('title', 'description')
+
