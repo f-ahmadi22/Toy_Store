344e54a9375866b145602a7767f27555e420b6a3
CE9752 <fateme.ahmadi1522@gmail.com>
2024-02-17 20:35:19 +0330

    Add product Category model

--- toystore/store/models.py
+++ toystore/store/models.py
@@ -1,3 +1,19 @@
 from django.db import models
-
+from ..blog.models import MyBaseModel
 # Create your models here.
+
+
+class Category(MyBaseModel):
+    title = models.CharField(max_length=250, unique=True, null=False, blank=False, verbose_name="title")
+    description = models.TextField(null=False, blank=False, verbose_name="description")
+
+    class Meta:
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
+        ordering = ['id']
+
+    def __str__(self):
+        return self.title
+
+
+
