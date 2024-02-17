ccba1920ceed1e2b0a7e85e9931660062c6bf2bb
CE9752 <fateme.ahmadi1522@gmail.com>
2024-02-17 11:27:22 +0330

    Create PostAdmin for admin panel

--- toystore/blog/admin.py
+++ toystore/blog/admin.py
@@ -11,3 +11,13 @@ class CategoryAdmin(admin.ModelAdmin):
     list_filter = ('is_active',)
     list_editable = ('is_active',)
     search_fields = ('title', 'description')
+
+
+@register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'description', 'category', 'is_active', 'created_at', 'updated_at')
+    list_display_links = ('id', 'title', 'description', 'category', 'is_active', 'created_at', 'updated_at')
+    list_filter = ('category',)
+    list_editable = ('is_active',)
+    search_fields = ('title', 'description', 'category__title', 'category__description')
+
