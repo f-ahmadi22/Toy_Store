1a9242bcf12d3264d148577339595e9ab781f93c
CE9752 <fateme.ahmadi1522@gmail.com>
2024-02-17 11:47:54 +0330

    Create CommentAdmin for admin panel

--- toystore/blog/admin.py
+++ toystore/blog/admin.py
@@ -7,7 +7,7 @@ from .models import Category, Post, Comment, Media
 @register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('id', 'title', 'description', 'is_active', 'created_at', 'updated_at')
-    list_display_links = ('id', 'title', 'description', 'is_active', 'created_at', 'updated_at')
+    list_display_links = ('id', 'title', 'description')
     list_filter = ('is_active',)
     list_editable = ('is_active',)
     search_fields = ('title', 'description')
@@ -16,8 +16,16 @@ class CategoryAdmin(admin.ModelAdmin):
 @register(Post)
 class PostAdmin(admin.ModelAdmin):
     list_display = ('id', 'title', 'description', 'category', 'is_active', 'created_at', 'updated_at')
-    list_display_links = ('id', 'title', 'description', 'category', 'is_active', 'created_at', 'updated_at')
+    list_display_links = ('id', 'title', 'description', 'category')
     list_filter = ('category',)
     list_editable = ('is_active',)
     search_fields = ('title', 'description', 'category__title', 'category__description')
 
+
+@register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'post', 'author', 'content', 'is_approved', 'is_active', 'created_at', 'updated_at')
+    list_display_links = ('id', 'post', 'author', 'content',)
+    list_filter = ('post', 'post__category', 'author', 'is_approved', 'is_active')
+    list_editable = ('is_active', 'is_approved')
+    search_fields = ('post__title', 'post__category', 'content', 'author')
--- toystore/blog/models.py
+++ toystore/blog/models.py
@@ -41,7 +41,7 @@ class Post(MyBaseModel):
 
 
 class Comment(MyBaseModel):
-    post = models.ForeignKey(Post, related_name="comments", on_delete=models, verbose_name="post")
+    post = models.ForeignKey(Post, related_name="comments", on_delete=models.CASCADE, verbose_name="post")
     author = models.CharField(max_length=250, null=False, blank=False, verbose_name="author")
     content = models.TextField(null=False, blank=False, verbose_name="content")
     is_approved = models.BooleanField(default=False, verbose_name="is_approved")
