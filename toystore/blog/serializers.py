7b147b7950c22e6fdb74d41263470bc44a57fcb0
CE9752 <fateme.ahmadi1522@gmail.com>
2024-02-18 10:52:25 +0330

    Add LoginSerializers in Blog app

--- toystore/blog/serializers.py
+++ toystore/blog/serializers.py
@@ -1,5 +1,9 @@
 from rest_framework import serializers
 from .models import BlogCategory, Post, BlogComment, BlogMedia
+from django.contrib.auth import authenticate
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 
 class CategorySerializer(serializers.ModelSerializer):
@@ -26,3 +30,32 @@ class MediaSerializer(serializers.ModelSerializer):
     class Meta:
         model = BlogMedia
         fields = '__all__'
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = '__all__'
+
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField()
+
+    def validate(self, data):
+        username = data.get('username')
+        password = data.get('password')
+
+        if username and password:
+            user = authenticate(username=username, password=password)
+            if user:
+                if not user.is_active:
+                    raise serializers.ValidationError("User account is disabled.")
+                data['user'] = user
+            else:
+                raise serializers.ValidationError("Unable to log in with provided credentials.")
+        else:
+            raise serializers.ValidationError("Must include 'email' and 'password'.")
+
+        return data
+    
