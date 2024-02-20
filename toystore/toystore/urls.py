d0f0c0efd2f577b8f2dce936dafdc5a0abce8a9b
CE9752 <fateme.ahmadi1522@gmail.com>
2024-02-20 13:57:00 +0330

    Auth app urls

--- toystore/toystore/urls.py
+++ toystore/toystore/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls')),
     path('store/', include('store.urls')),
+    path('auth/', include('authentication.urls')),
 ]
