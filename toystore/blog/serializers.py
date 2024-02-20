7a489c79162e419ddef9c6f9baeac251ba7a30d9
CE9752 <fateme.ahmadi1522@gmail.com>
2024-02-20 13:34:28 +0330

    LoginSerializer implementation

--- toystore/blog/serializers.py
+++ toystore/blog/serializers.py
@@ -31,31 +31,3 @@ class MediaSerializer(serializers.ModelSerializer):
     class Meta:
         model = BlogMedia
         fields = '__all__'
-
-
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = '__all__'
-
-
-class LoginSerializer(serializers.Serializer):
-    username = serializers.CharField()
-    password = serializers.CharField()
-
-    def validate(self, data):
-        username = data.get('username')
-        password = data.get('password')
-
-        if username and password:
-            user = authenticate(username=username, password=password)
-            if user:
-                if not user.is_active:
-                    raise serializers.ValidationError("User account is disabled.")
-                data['user'] = user
-            else:
-                raise serializers.ValidationError("Unable to log in with provided credentials.")
-        else:
-            raise serializers.ValidationError("Must include 'username' and 'password'.")
-
-        return data
