5f22405f01abba99f63c6351aaf5c26267266bdb
CE9752 <fateme.ahmadi1522@gmail.com>
2024-02-21 23:01:11 +0330

    README file completed

--- toystore/authentication/urls.py
+++ toystore/authentication/urls.py
@@ -2,11 +2,7 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import UserViewSet, LoginAPIView, SignupAPIView
 
-router = DefaultRouter()
-router.register(r'users', UserViewSet, basename='user')
-
 urlpatterns = [
-    path('', include(router.urls)),
     path('login/', LoginAPIView.as_view(), name='login'),
     path('signup/', SignupAPIView.as_view(), name='signup'),
 ]
